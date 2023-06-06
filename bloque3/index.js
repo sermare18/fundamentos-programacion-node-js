e8bc646cda5e20a18839e51acd4f9254bf6a7db0
Sergio Martín <sergio.mare2002@gmail.com>
2023-06-07 00:23:58 +0200

    Instalando librerias de terceros

---
+++ bloque3/index.js
@@ -0,0 +1,9 @@
+// Para importar librerías tanto de terceros como de Node
+const readline = require('readline');
+const moment = require('moment');
+
+// El comando: npm init, se utiliza a la hora de inicializar un proyecto JS, con él se crea el fichero package.json
+// que registra todas las dependencias a librerías tanto de terceros como de node que hemos incluido en nuestro proyecto.
+
+// Con el comando: npm install, instalamos todas las dependencias necesarias incluidas en el fichero package.json
+// Con el comando: npm install {nombre_librería} --save, instalamos la librería en cuestión y la registramos en el fichero package.json
