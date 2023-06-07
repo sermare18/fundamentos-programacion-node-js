c319c6cfe4ebe3a4cdfff3203b84b4081e68dfba
Sergio Martín <sergio.mare2002@gmail.com>
2023-06-07 13:14:06 +0200

    Utilizando libreria readline

--- bloque3/index.js
+++ bloque3/index.js
@@ -7,3 +7,39 @@ const moment = require('moment');
 
 // Con el comando: npm install, instalamos todas las dependencias necesarias incluidas en el fichero package.json
 // Con el comando: npm install {nombre_librería} --save, instalamos la librería en cuestión y la registramos en el fichero package.json
+
+let rl = readline.createInterface(
+    process.stdin,
+    process.stdout
+);
+
+// Con arrow function
+/*
+rl.question('What is your age? ', (age) => {
+    console.log('Your age is ' + age);
+
+    rl.close();
+});
+*/
+
+rl.question('¿Que edad tines? ', function (edad) {
+    rl.question('¿Cúal es tu nomre? ', function (nombre) {
+        if (edad < 18) {
+            console.log('Eres menor de edad ' + nombre + ', largo de aquí.');
+        } else {
+            console.log('Pase, pase, gracias por venir 🚀 ' + nombre + ', eres mayor de edad.');
+        }
+
+        rl.question('¿Cuántas iteraciones quieres hacer? ', function (iteraciones) {
+            for (let i = 0; i < iteraciones; i++) {
+                console.log(i);
+            }
+
+            // Cierra la interfaz del readline
+            rl.close()
+        });
+    })
+});
+
+// El pasar una función por parámetro de otra función es lo que se conoce como callback, siempre que se utilicen para recoger datos (En este caso la edad del usuario).
+
