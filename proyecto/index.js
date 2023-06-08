4ea2ee558bd0cf21a03cd97a5e36669ad95c02de
Sergio Martín <sergio.mare2002@gmail.com>
2023-06-08 21:05:32 +0200

    Marcando las tareas

--- proyecto/index.js
+++ proyecto/index.js
@@ -12,15 +12,15 @@ function addTask(taskList, taskDescription) {
 }
 
 function printTaskList(taskList) {
-    // [] Sacar la basura
-    // [x] Lavar los platos
+    // 1. [] Sacar la basura
+    // 2. [x] Lavar los platos
     for (let i = 0; i < taskList.length; i++) {
         if (taskList[i].done) {
             // tarea realizada
-            console.log('[x] ' + taskList[i].description);
+            console.log((i + 1) + '. [x] ' + taskList[i].description);
         } else {
             // tarea no realizada
-            console.log('[ ] ' + taskList[i].description);
+            console.log((i + 1) + '. [ ] ' + taskList[i].description);
         }
     }
 }
@@ -34,12 +34,12 @@ function printTaskList(taskList) {
 
 // Primer modo: lectura de tareas necesarias
 
-function askForTasks(taskList) {
+function mode1(taskList) {
     rl.question('Introduce una nueva tarea.\nIntroduce fin si ya no deseas introducir nuevas tareas: ', function(taskDesc) {
         switch (taskDesc) {
             case 'fin':
                 console.log('No se introducen ya nuevas tareas');
-                rl.close();
+                mode2(taskList);
                 break;
             case 'exit':
                 rl.close();
@@ -49,12 +49,50 @@ function askForTasks(taskList) {
                 console.log('La lista de tareas actual es: ');
                 printTaskList(taskList);
                 // Función recursiva
-                askForTasks(taskList);
+                mode1(taskList);
         }
     });
 }
 
-askForTasks(taskList);
-
-
 // Segundo modo: marcar las tareas realizadas
+function markTaskDone(taskList, index) {
+    if (index >= 0 && index < taskList.length) {
+        taskList[index].done = true;
+    } else {
+        console.log('Invalid task number');
+    }
+}
+
+function checkAllDone(taskList) {
+    for (let task of taskList) {
+        if (!task.done) return false;
+    }
+    return true;
+}
+
+function mode2(taskList) {
+    printTaskList(taskList);
+    rl.question('¿Qué tareas ya has realizado? (1-N) ', function(taskNumber) {
+        switch (taskNumber) {
+            case 'fin':
+            case 'exit':
+                // Cerrar el programa
+                console.log('Bye bye');
+                rl.close();
+                break;
+            default:
+                markTaskDone(taskList, taskNumber - 1);
+                // Comprobar si estan todas las tareas hechas y cerrar el programa
+                if (checkAllDone(taskList)) {
+                    console.log('Muy bien! Has completado todo el trabajo!');
+                    rl.close();
+                } else {
+                    // Función recursiva
+                    mode2(taskList);
+                }
+        }
+    });
+}
+
+// Iniciamos el programa
+mode1(taskList);
