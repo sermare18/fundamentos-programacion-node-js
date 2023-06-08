b266bb40d3f932ecab097bf239b07a88712ce4a3
Sergio Martín <sergio.mare2002@gmail.com>
2023-06-08 20:29:45 +0200

    Pidiendo al usuario las tareas

--- proyecto/index.js
+++ proyecto/index.js
@@ -1,4 +1,8 @@
 const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 // Tarea: valor booleano y descripción
 let taskList = [];
@@ -21,17 +25,36 @@ function printTaskList(taskList) {
     }
 }
 
-addTask(taskList, "Hacer curso JS de Mastermind");
-addTask(taskList, "Lavar los platos");
-addTask(taskList, "Hacer stream");
-addTask(taskList, "Hacer video de youtube");
-addTask(taskList, "Hacer la cena");
-addTask(taskList, "Irse a dormir");
-
-printTaskList(taskList);
+// addTask(taskList, "Hacer curso JS de Mastermind");
+// addTask(taskList, "Lavar los platos");
+// addTask(taskList, "Hacer stream");
+// addTask(taskList, "Hacer video de youtube");
+// addTask(taskList, "Hacer la cena");
+// addTask(taskList, "Irse a dormir");
 
 // Primer modo: lectura de tareas necesarias
 
+function askForTasks(taskList) {
+    rl.question('Introduce una nueva tarea.\nIntroduce fin si ya no deseas introducir nuevas tareas: ', function(taskDesc) {
+        switch (taskDesc) {
+            case 'fin':
+                console.log('No se introducen ya nuevas tareas');
+                rl.close();
+                break;
+            case 'exit':
+                rl.close();
+                break;
+            default:
+                addTask(taskList, taskDesc);
+                console.log('La lista de tareas actual es: ');
+                printTaskList(taskList);
+                // Función recursiva
+                askForTasks(taskList);
+        }
+    });
+}
+
+askForTasks(taskList);
 
 
 // Segundo modo: marcar las tareas realizadas
